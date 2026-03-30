82b6681c5035faabcedc60acd5def2addc187fd9
kantiankant <qwerasdnoob@gmail.com>
2026-03-30 18:52:11 +0800

    Removed some unnecessary comments

--- kisswm/src/config.h
+++ kisswm/src/config.h
@@ -1,10 +1,10 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-#define GAP             8       /* gap between windows, pixels        */
-#define BAR_HEIGHT      24        /* height of your status bar          */
-#define BORDER_WIDTH    1         /* window border thickness, pixels     */
-#define BORDER_FOCUS    0xfcfbf9 /* focused window border colour        */
-#define BORDER_NORMAL   0x000000 /* unfocused window border colour      */
+#define GAP             8  
+#define BAR_HEIGHT      24  
+#define BORDER_WIDTH    1  
+#define BORDER_FOCUS    0xfcfbf9
+#define BORDER_NORMAL   0x000000
 
-#endif /* CONFIG_H */
+#endif 
